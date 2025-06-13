70f5c248631fce612e5e2a34a7322f5bd55ac16a
Cory Adkins <adkins.cory@gmail.com>
2025-06-13 16:19:41 -0400

    refactor build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,10 @@
 
 set -ouex pipefail
 
+log() {
+  echo "=== $* ==="
+}
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -9,15 +13,37 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# fedora official packages
-dnf5 install -y R
+# RPM packages list
+declare -A RPM_PACKAGES=(
+  ["fedora"]="\
+    neovim \
+    r"
 
-# copr packages 
+  ["copr:iucar/rstudio"]="rstudio-desktop"
 
-dnf5 -y copr enable iucar/rstudio
-dnf5 -y copr enable iucar/duckdb
-dnf5 -y install rstudio-desktop
-dnf5 -y install duckdb
+  ["copr:iucar/duckdb"]="duckdb"
+)
+# adapted from astrovm/amyos install-apps.sh
+log "Installing RPM packages"
+
+mkdir -p /var/opt
+for repo in "${!RPM_PACKAGES[@]}"; do
+  read -ra pkg_array <<<"${RPM_PACKAGES[$repo]}"
+  if [[ $repo == copr:* ]]; then
+    # Handle COPR packages
+    copr_repo=${repo#copr:}
+    dnf5 -y copr enable "$copr_repo"
+    dnf5 -y install "${pkg_array[@]}"
+    dnf5 -y copr disable "$copr_repo"
+  else
+    # Handle regular packages
+    [[ $repo != "fedora" ]] && enable_opt="--enable-repo=$repo" || enable_opt=""
+    cmd=(dnf5 -y install)
+    [[ -n "$enable_opt" ]] && cmd+=("$enable_opt")
+    cmd+=("${pkg_array[@]}")
+    "${cmd[@]}"
+  fi
+done
 
 #### Example for enabling a System Unit File
 
