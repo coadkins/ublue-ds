be7456412f20d62142a5802d1f25ba8c851ae3a1
Cory Adkins <adkins.cory@gmail.com>
2025-06-13 14:50:03 -0400

    add copr packages
    cleanup

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,17 +9,15 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
 # fedora official packages
 dnf5 install -y R
+
+# copr packages 
+
+dnf5 -y copr enable iucar/rstudio
+dnf5 -y copr enable iucar/duckdb
+dnf5 -y install rstudio-desktop
+dnf5 -y install duckdb
 
 #### Example for enabling a System Unit File
 
