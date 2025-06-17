cdacfd506f167fcf9a3081b8e8dca2130786936b
Cory Adkins <adkins.cory@gmail.com>
2025-06-16 20:53:32 -0400

    added positron (hard-coded version)

--- build_files/build.sh
+++ build_files/build.sh
@@ -47,6 +47,16 @@ for repo in "${!RPM_PACKAGES[@]}"; do
   fi
 done
 
+# binaries
+POSITRON_V=2025.07.0-112
+LINUX_ARCH=x64
+
+POSITRON_URL="https://cdn.posit.co/positron/dailies/rpm/x86_64/Positron-${POSITRON_V}-${LINUX_ARCH}.rpm"
+
+wget "$POSITRON_URL" -O positron.rpm
+dnf5 -y install positron.rpm
+rm positron.rpm
+
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
