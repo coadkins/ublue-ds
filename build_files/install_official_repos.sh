f94e5db50b0a48ac9c56f43756ef3fce94a8b38b
Cory Adkins <adkins.cory@gmail.com>
2025-06-18 21:06:35 -0400

    add vscodium
    amend codium script

--- build_files/install_official_repos.sh
+++ build_files/install_official_repos.sh
@@ -4,4 +4,7 @@ set -ouex pipefail
 
 echo "installing packages from official fedora repos"
 dnf5 install -y \
-	neovim
+	neovim 
+
+echo "installing vscodium"
+dnf5 install --enable-repo=download.vscodium.com -y codium
