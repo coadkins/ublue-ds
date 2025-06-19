f9171a5150d1da00e2859db41a0fa0d2a9302482
Cory Adkins <adkins.cory@gmail.com>
2025-06-18 21:06:35 -0400

    add vscodium
    amend codium script

--- build_files/install_official_repos.sh
+++ build_files/install_official_repos.sh
@@ -4,4 +4,4 @@ set -ouex pipefail
 
 echo "installing packages from official fedora repos"
 dnf5 install -y \
-	neovim
+	neovim 
---
+++ build_files/install_other_repos.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#!/bin/bash
+echo "add vscodium rpm keys and repo"
+rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | sudo tee -a /etc/yum.repos.d/vscodium.repo
+echo "installing vscodium"
+dnf5 install -y codium
+echo "disabling vscodium repo"
+dnf5 config-manager setopt gitlab.com_paulcarroty_vscodium_repo.enabled=0
+
